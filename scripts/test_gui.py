78af6f8952e009aef278f0bd828b8869cd63d1c7
MattiaRacca <mattia.rh@gmail.com>
2019-07-15 16:21:17 +0300

    initial very crude integration widget-interpreter

--- scripts/test_gui.py
+++ scripts/test_gui.py
@@ -3,17 +3,20 @@
 import sys
 import os
 import rospkg
+import rospy
+import time
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QSlider, QApplication, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QApplication, QPushButton
 from PyQt5.QtCore import Qt
 
-import panda_eup.panda_primitive as eup_pp
+import panda_eup.panda_primitive as pp
 from panda_gui.panda_widgets import PandaProgramWidget
 
 
 class TestGui(QWidget):
-    def __init__(self):
+    def __init__(self, interpreter):
         super(TestGui, self).__init__()
+        self.interpreter = interpreter
         self.initUI()
 
     def initUI(self):
@@ -21,28 +24,126 @@ class TestGui(QWidget):
 
         self.vbox = QVBoxLayout()
         self.vbox.setAlignment(Qt.AlignTop)
-
-        self.button = QPushButton("add primitive")
-        self.sld = QSlider(Qt.Horizontal)
-        self.sld.setFocusPolicy(Qt.NoFocus)
-        self.sld.setRange(1, 100)
-        self.sld.setMaximumSize(100,120)
-        self.sld.setValue(50)
+        self.low_buttons = QWidget()
+        self.low_buttons_layout = QHBoxLayout()
+        self.low_buttons_layout.setAlignment(Qt.AlignCenter)
 
         self.panda_program_widget = PandaProgramWidget(self)
-        # self.button.clicked.connect(self.panda_program_widget.loadPandaProgram(current_program))
+        self.go_start_button = QPushButton("Go to start pose")
+        self.execute_one_step_button = QPushButton("Execute one step")
+        self.revert_one_step_button = QPushButton("Revert one step")
+
+        self.go_start_button.clicked.connect(self.go_to_starting_state)
+        self.execute_one_step_button.clicked.connect(self.execute_one_step)
+        self.revert_one_step_button.clicked.connect(self.revert_one_step)
+
+        self.low_buttons_layout.addWidget(self.go_start_button)
+        self.low_buttons_layout.addWidget(self.execute_one_step_button)
+        self.low_buttons_layout.addWidget(self.revert_one_step_button)
+        self.low_buttons.setLayout(self.low_buttons_layout)
 
         self.vbox.addWidget(self.panda_program_widget)
-        self.vbox.addWidget(self.sld)
-        self.vbox.addWidget(self.button)
+        self.vbox.addWidget(self.low_buttons)
 
         self.setLayout(self.vbox)
         self.show()
 
+    def go_to_starting_state(self):
+        self.interpreter.go_to_starting_state()
+        self.panda_program_widget.updateWidget()
+    def execute_one_step(self):
+        self.interpreter.execute_one_step()
+        self.panda_program_widget.updateWidget()
+    def revert_one_step(self):
+        self.interpreter.revert_one_step()
+        self.panda_program_widget.updateWidget()
+
+
+class DebugInterpreter(object):
+    def __init__(self):
+        super(DebugInterpreter, self).__init__()
+        self.loaded_program = None
+        self.next_primitive_index = -1
+
+    def load_program(self, program):
+        self.loaded_program = program
+        self.next_primitive_index = -1
+        for primitive in program.primitives:
+            primitive.status = pp.PandaPrimitiveStatus.NEUTRAL
+
+    def go_to_starting_state(self):
+        if self.loaded_program is None:
+            return False
+        self.next_primitive_index = 0
+        for primitive in self.loaded_program.primitives:
+            primitive.status = pp.PandaPrimitiveStatus.NEUTRAL
+        return True
+
+    def execute_one_step(self):
+        if self.loaded_program is None:
+            rospy.logwarn('no program loaded')
+            return False
+
+        try:
+            primitive_to_execute = self.loaded_program.get_nth_primitive(self.next_primitive_index)
+        except pp.PandaProgramException:
+            rospy.logwarn('Nothing left to execute OR Empty program OR wrong indexing')
+            return False
+
+        primitive_to_execute.status = pp.PandaPrimitiveStatus.EXECUTING
+        time.sleep(3)
+        result = True
+
+        if result:
+            rospy.loginfo('Executed primitive ' + primitive_to_execute.__str__())
+            primitive_to_execute.status = pp.PandaPrimitiveStatus.EXECUTED
+            self.next_primitive_index += 1
+        else:
+            rospy.logerr('Error while executing ' + primitive_to_execute.__str__())
+            primitive_to_execute.status = pp.PandaPrimitiveStatus.ERROR
+            return False
+        return True
+
+    def revert_one_step(self):
+        if self.loaded_program is None:
+            rospy.logwarn('no program loaded')
+            return False
+
+        try:
+            primitive_to_revert = self.loaded_program.get_nth_primitive(self.next_primitive_index - 1)
+        except pp.PandaProgramException:
+            rospy.logwarn('Nothing left to revert OR Empty program OR wrong indexing')
+            return False
+
+        if not primitive_to_revert.revertible:
+            rospy.logwarn('Cannot revert this primitive! the previous primitives was not completely updated...')
+            primitive_to_revert.status = pp.PandaPrimitiveStatus.ERROR
+            return False
+
+        primitive_to_revert.status = pp.PandaPrimitiveStatus.REVERTING
+        time.sleep(3)
+        result =True
+
+        if result:
+            rospy.loginfo('Reverted primitive ' + primitive_to_revert.__str__())
+            primitive_to_revert.status = pp.PandaPrimitiveStatus.NEUTRAL
+            self.next_primitive_index -= 1
+        else:
+            rospy.logerr('Error while reverting ' + primitive_to_revert.__str__())
+            primitive_to_revert.status = pp.PandaPrimitiveStatus.ERROR
+            return False
+
+        return True
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = TestGui()
+
+    interpreter = DebugInterpreter()
     program_path = os.path.join(rospkg.RosPack().get_path('panda_pbd'), 'resources')
-    ex.panda_program_widget.loadPandaProgram(eup_pp.load_program_from_file(program_path, 'program.pkl'))
+    interpreter.load_program(pp.load_program_from_file(program_path, 'program.pkl'))
+
+    ex = TestGui(interpreter)
+    ex.panda_program_widget.loadPandaInterpreter(interpreter)
     # ex.showMaximized()
     sys.exit(app.exec_())
--- src/panda_gui/panda_widgets.py
+++ src/panda_gui/panda_widgets.py
@@ -19,6 +19,17 @@ V_SPACING = 20
 # Minimum number of visible primitives on screen
 MIN_PRIMITIVE = 5
 
+# Color Palettes
+gray_palette = QPalette()
+gray_palette.setColor(QPalette.Background, QColor("gainsboro"))
+white_palette = QPalette()
+white_palette.setColor(QPalette.Background, QColor("ghostwhite"))
+current_index_palette = QPalette()
+current_index_palette.setColor(QPalette.Background, QColor("lightskyblue"))
+executed_primitive_palette = QPalette()
+executed_primitive_palette.setColor(QPalette.Background, QColor("cornflowerblue"))
+
+
 class PandaProgramWidget(QWidget):
     def __init__(self, parent):
         super(PandaProgramWidget, self).__init__(parent)
@@ -26,7 +37,8 @@ class PandaProgramWidget(QWidget):
 
     def initUI(self):
         self.program = None
-        self.program_size = 0
+        self.primitive_widget_list = []
+        program_size = 0
 
         # Create layout for program widget and add Scroll Area
         self.widget_layout = QHBoxLayout(self)
@@ -46,30 +58,36 @@ class PandaProgramWidget(QWidget):
         self.program_widget.setSizePolicy(sizePolicy)
 
         # Color the Program area
-        palette = QPalette()
-        palette.setColor(QPalette.Background, QColor("gainsboro"))
         self.setAutoFillBackground(True)
-        self.setPalette(palette)
+        self.setPalette(gray_palette)
         self.program_widget.setAutoFillBackground(True)
-        self.program_widget.setPalette(palette)
+        self.program_widget.setPalette(gray_palette)
 
         # Add scrolling area
         self.program_scroll_area.setWidget(self.program_widget)
-        self.program_widget.setGeometry(0, 0, (H_SPACING + PRIMITIVE_WIDTH)*self.program_size, V_SPACING + PRIMITIVE_HEIGHT)
+        self.program_widget.setGeometry(0, 0, (H_SPACING + PRIMITIVE_WIDTH)*program_size, V_SPACING + PRIMITIVE_HEIGHT)
         # self.program_scroll_area.resize(self.sizeHint())
 
     def addPrimitiveWidget(self, primitive):
-        primitive_widget = PandaPrimitiveWidget(self.program_widget, primitive) # TODO: before it was self only
+        primitive_widget = PandaPrimitiveWidget(self.program_widget, primitive)
+        self.primitive_widget_list.append(primitive_widget)
         self.program_widget_layout.addWidget(primitive_widget)
 
         program_length = self.program.get_program_length()
         if self.program_widget.width() < (H_SPACING + PRIMITIVE_WIDTH)*program_length:
             self.program_widget.setGeometry(0, 0, (H_SPACING + PRIMITIVE_WIDTH)*program_length, V_SPACING + PRIMITIVE_HEIGHT)
 
-    def loadPandaProgram(self, program):
-        self.program = program
+    def loadPandaInterpreter(self, interpreter):
+        # TODO: self.program is redundant
+        self.program = interpreter.loaded_program
+        self.interpreter = interpreter
         for primitive in self.program.primitives:
             self.addPrimitiveWidget(primitive)
+        self.updateWidget()
+
+    def updateWidget(self):
+        for i, primitive_widget in enumerate(self.primitive_widget_list):
+            primitive_widget.updateWidget(i, self.interpreter.next_primitive_index)
 
     def sizeHint(self):
         return QSize((H_SPACING+PRIMITIVE_WIDTH)*MIN_PRIMITIVE, V_SPACING*3 + PRIMITIVE_HEIGHT)
@@ -85,8 +103,9 @@ class PandaPrimitiveWidget(QFrame):
 
     def initUI(self, panda_primitive):
         # Create widget subcomponents
+        self.primitive = panda_primitive
         self.primitive_label = QLabel()
-        self.status_label = QLabel(self.tr("Status Y"))
+        self.status_label = QLabel(str(panda_primitive.status.name))
 
         primitive_icon_path = os.path.join(rospkg.RosPack().get_path('panda_pbd'), 'resources',
                                            panda_primitive.__class__.__name__ + '.png')
@@ -105,10 +124,18 @@ class PandaPrimitiveWidget(QFrame):
         self.setFrameShape(QFrame.Panel)
         self.setFrameShadow(QFrame.Raised)
         self.setLineWidth(2)
-        palette = QPalette()
-        palette.setColor(QPalette.Background, QColor("ghostwhite"))
+
         self.setAutoFillBackground(True)
-        self.setPalette(palette)
+        self.setPalette(white_palette)
 
     def sizeHint(self):
         return QSize(PRIMITIVE_WIDTH, PRIMITIVE_HEIGHT)
+
+    def updateWidget(self, own_index, program_current_index):
+        self.status_label.setText(str(self.primitive.status.name))
+        if  own_index < program_current_index:
+            self.setPalette(executed_primitive_palette)
+        elif own_index > program_current_index:
+            self.setPalette(gray_palette)
+        else:
+            self.setPalette(current_index_palette)
