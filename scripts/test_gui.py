992fd8257715381e093f1f89b3e94040b72ec943
MattiaRacca <mattia.rh@gmail.com>
2019-07-16 17:14:35 +0300

    started working on main widget

--- scripts/test_gui.py
+++ scripts/test_gui.py
@@ -11,53 +11,7 @@ from PyQt5.QtCore import Qt
 
 import panda_eup.panda_primitive as pp
 from panda_eup.program_interpreter import PandaProgramInterpreter
-from panda_gui.panda_widgets import PandaProgramWidget
-
-
-class TestGui(QWidget):
-    def __init__(self, interpreter):
-        super(TestGui, self).__init__()
-        self.interpreter = interpreter
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('EUP widget')
-
-        self.vbox = QVBoxLayout()
-        self.vbox.setAlignment(Qt.AlignTop)
-        self.low_buttons = QWidget()
-        self.low_buttons_layout = QHBoxLayout()
-        self.low_buttons_layout.setAlignment(Qt.AlignCenter)
-
-        self.panda_program_widget = PandaProgramWidget(self)
-        self.go_start_button = QPushButton("Go to start pose")
-        self.execute_one_step_button = QPushButton("Execute one step")
-        self.revert_one_step_button = QPushButton("Revert one step")
-
-        self.go_start_button.clicked.connect(self.go_to_starting_state)
-        self.execute_one_step_button.clicked.connect(self.execute_one_step)
-        self.revert_one_step_button.clicked.connect(self.revert_one_step)
-
-        self.low_buttons_layout.addWidget(self.go_start_button)
-        self.low_buttons_layout.addWidget(self.execute_one_step_button)
-        self.low_buttons_layout.addWidget(self.revert_one_step_button)
-        self.low_buttons.setLayout(self.low_buttons_layout)
-
-        self.vbox.addWidget(self.panda_program_widget)
-        self.vbox.addWidget(self.low_buttons)
-
-        self.setLayout(self.vbox)
-        self.show()
-
-    def go_to_starting_state(self):
-        self.interpreter.go_to_starting_state()
-        self.panda_program_widget.updateWidget()
-    def execute_one_step(self):
-        self.interpreter.execute_one_step()
-        self.panda_program_widget.updateWidget()
-    def revert_one_step(self):
-        self.interpreter.revert_one_step()
-        self.panda_program_widget.updateWidget()
+from panda_gui.panda_widgets import EUPWidget
 
 
 if __name__ == '__main__':
@@ -68,7 +22,8 @@ if __name__ == '__main__':
     program_path = os.path.join(rospkg.RosPack().get_path('panda_pbd'), 'resources')
     interpreter.load_program(pp.load_program_from_file(program_path, 'program.pkl'))
 
-    ex = TestGui(interpreter)
+    ex = EUPWidget()
     ex.panda_program_widget.loadPandaInterpreter(interpreter)
+    ex.show()
     # ex.showMaximized()
     sys.exit(app.exec_())
--- src/panda_gui/panda_widgets.py
+++ src/panda_gui/panda_widgets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from __future__ import division
 
-from PyQt5.QtWidgets import QWidget, QLabel, QFrame, QLayout, QGridLayout, QHBoxLayout, QVBoxLayout, QScrollArea, QSizePolicy
+from PyQt5.QtWidgets import QWidget, QLabel, QFrame, QPushButton, QHBoxLayout, QVBoxLayout, QScrollArea, QSizePolicy, QGroupBox
 from PyQt5.QtCore import Qt, QObject, pyqtSignal, QSize
 from PyQt5.QtGui import QColor, QPalette, QPixmap
 
@@ -14,7 +14,7 @@ PRIMITIVE_HEIGHT = 120
 
 # Spacings for the Program Widget
 H_SPACING = 20
-V_SPACING = 20
+V_SPACING = 30
 
 # Minimum number of visible primitives on screen
 MIN_PRIMITIVE = 5
@@ -30,7 +30,68 @@ executed_primitive_palette = QPalette()
 executed_primitive_palette.setColor(QPalette.Background, QColor("cornflowerblue"))
 
 
-class PandaProgramWidget(QWidget):
+class EUPWidget(QWidget):
+    def __init__(self):
+        super(EUPWidget, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('EUP Widget')
+
+        self.vbox = QVBoxLayout(self)
+        self.vbox.setAlignment(Qt.AlignTop)
+
+        self.low_buttons = QWidget()
+        self.low_buttons_layout = QHBoxLayout()
+        self.low_buttons_layout.setAlignment(Qt.AlignCenter)
+
+        self.panda_program_widget = PandaProgramWidget(self)
+        self.adjust_parameters_frame = QGroupBox(self)
+        self.adjust_parameters_frame.setTitle('Primitive parameters:')
+        self.go_start_button = QExpandingPushButton("Go to start pose")
+        self.execute_one_step_button = QExpandingPushButton("Execute one step")
+        self.revert_one_step_button = QExpandingPushButton("Revert one step")
+        self.execute_rest_button = QExpandingPushButton("Execute rest of program")
+        self.revert_to_beginning_button = QExpandingPushButton("Revert to beginning")
+
+        size_policy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        self.adjust_parameters_frame.setSizePolicy(size_policy)
+
+        self.go_start_button.clicked.connect(self.go_to_starting_state)
+        self.execute_one_step_button.clicked.connect(self.execute_one_step)
+        self.revert_one_step_button.clicked.connect(self.revert_one_step)
+
+        self.low_buttons_layout.addWidget(self.go_start_button)
+        self.low_buttons_layout.addWidget(QVerticalLine())
+        self.low_buttons_layout.addWidget(self.execute_one_step_button)
+        self.low_buttons_layout.addWidget(self.revert_one_step_button)
+        self.low_buttons_layout.addWidget(QVerticalLine())
+        self.low_buttons_layout.addWidget(self.execute_rest_button)
+        self.low_buttons_layout.addWidget(self.revert_to_beginning_button)
+        self.low_buttons.setLayout(self.low_buttons_layout)
+
+        self.vbox.addWidget(self.panda_program_widget)
+        self.vbox.addWidget(self.adjust_parameters_frame)
+        self.vbox.addWidget(self.low_buttons)
+
+    def go_to_starting_state(self):
+        self.panda_program_widget.interpreter.go_to_starting_state()
+        self.panda_program_widget.updateWidget()
+    def execute_one_step(self):
+        self.panda_program_widget.interpreter.execute_one_step()
+        self.panda_program_widget.updateWidget()
+    def revert_one_step(self):
+        self.panda_program_widget.interpreter.revert_one_step()
+        self.panda_program_widget.updateWidget()
+
+    def sizeHint(self):
+        return QSize(1280, 720)
+
+    def minimumSizeHint(self):
+        return QSize(1280, 720)
+
+
+class PandaProgramWidget(QGroupBox):
     def __init__(self, parent):
         super(PandaProgramWidget, self).__init__(parent)
         self.initUI()
@@ -39,6 +100,7 @@ class PandaProgramWidget(QWidget):
         self.program = None
         self.primitive_widget_list = []
         program_size = 0
+        self.setTitle('No program loaded yet')
 
         # Create layout for program widget and add Scroll Area
         self.widget_layout = QHBoxLayout(self)
@@ -81,6 +143,7 @@ class PandaProgramWidget(QWidget):
         # TODO: self.program is redundant
         self.program = interpreter.loaded_program
         self.interpreter = interpreter
+        self.setTitle(self.program.name + ':')
         for primitive in self.program.primitives:
             self.addPrimitiveWidget(primitive)
         self.updateWidget()
@@ -139,3 +202,15 @@ class PandaPrimitiveWidget(QFrame):
             self.setPalette(gray_palette)
         else:
             self.setPalette(current_index_palette)
+
+class QVerticalLine(QFrame):
+    def __init__(self, parent=None):
+        super(QVerticalLine, self).__init__(parent)
+        self.setFrameShape(QFrame.VLine)
+        self.setFrameShadow(QFrame.Sunken)
+
+class QExpandingPushButton(QPushButton):
+    def __init__(self, text='', parent=None):
+        super(QExpandingPushButton, self).__init__(text, parent)
+        sizePolicy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+        self.setSizePolicy(sizePolicy)
